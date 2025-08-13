b61737e0811b43a19f38dd61ee1a6bb4da28d5b2
Ali-robot6660 <khujaev.qurbonali6660@gmail.com>
2025-08-13 18:06:35 +0900

    Программа для отслеживания веса

--- Python черновик/projects.py
+++ Python черновик/projects.py
@@ -1,9 +1,39 @@
-for a in range(1, 150):
-    for b in range(a, 151):
-        for c in range(b, 151):
-            for d in range(c, 151):
-                for e in range(d, 151):
-                    if e ** 5 > a ** 5 + b ** 5 + c ** 5 + d ** 5:
-                        break
-                    if a ** 5 + b ** 5 + c ** 5 + d ** 5 == e ** 5:
-                        print(a, b, c, d, e, "=", a+b+c+d+e)
+#num = int(input())
+#binary = ''
+#while num > 0:
+#    binary = str(num % 2) + binary
+#    num //= 2
+#print(binary)
+
+#s = 'abcdefg'
+#print(s[::-3])
+
+s = input()
+a = 0
+for i in s:
+    if i.islower():
+        a += 1
+print(a)
+
+
+
+
+n = int(input())
+a = 0
+for i in range(n):
+    s = input()
+    if s.count('11') >= 3:
+        a += 1
+print(a)
+
+
+
+
+s = input()
+maX = 0
+b = 0
+for i in range(len(s)):
+    if s.count(s[i]) >= b:
+        b = s.count(s[i])
+        maX = s[i]
+print(maX)
