9fcabdbe8b76d803b3aa0a66a9420c0cda0b84cc
Ali-robot6660 <khujaev.qurbonali6660@gmail.com>
2025-08-16 15:40:19 +0900

    Project Factorial and Power

---
+++ factorial_and_power.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number for factorial or power calculation: "))
+vibor = input("Enter 'factorial' for factorial or 'power' for power calculation: ").lower()
+result = 1
+if vibor == 'factorial':
+    for i in range(1, n + 1):
+        result *= i
+    print("Factorial number", n, "is", result)
+elif vibor == 'power':
+    power1 = int(input("Enter the power: "))
+    for i in range(power1):
+        result *= n
+        print(n,"**", i+1, "=", result)
+else:
+    print("Invalid input")
